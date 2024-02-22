6ec0583a90d80b333dc36ba66037a122b8f45f2f
CharlesZy123 <151004740+CharlesZy123@users.noreply.github.com>
2024-02-22 09:25:35 +0800

    [charles fonzy] hello

--- welcome/index.php
+++ welcome/index.php
@@ -12,7 +12,7 @@ if (isset($_SESSION['user_id'])) {
                 <div class="row">
                     <div class="col-sm-12 mt-5 text-center">
                         <h1><i class="fas fa-search"></i></h1>
-                        <h2 class="mt-2 mb-4 edited-text">Welcome to Job <span class="color-b">Hunt</span></h2>
+                        <h2 class="mt-2 mb-4 edited-text">Welcome to Job <span class="color-b">Hub</span></h2>
                         <h5 class="mb-3 edited-text">The place you can go to find a job that is just suitable for you!</h5>
                         <p class="mb-3 edited-text">Please register an account to get started. Do it now so that your desired career will start today!</p>
                         <a href="register" class="mt-1 mb-2 btn btn-info">Start Now</a>
