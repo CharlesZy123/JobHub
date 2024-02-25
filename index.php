c608911d20f69f15b09266daef5b5f18d95e9964
CharlesZy123 <151004740+CharlesZy123@users.noreply.github.com>
2024-02-25 14:19:00 +0800

    updated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(!isset($_SESSION['user_id'])){
-   header("Location: welcome/index");
+   header("Location: welcome/");
 }else{
    if(isset($_SESSION['user_id'])){
       header("Location: user/dashboard");
